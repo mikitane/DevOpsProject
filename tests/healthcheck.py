94608f4abcc6dee4a85aabfefb80b100039af920
mikitane <miika.luiro@etasku.fi>
2020-11-20 01:11:27 +0200

    Add test healthcheck

--- tests/healthcheck.py
+++ tests/healthcheck.py
@@ -1,2 +1,19 @@
-for i in range(20):
-  print('HEALTHY?')
+import requests
+import time
+
+retries = 0
+while True:
+	print("ASD")
+
+	try:
+		requests.get('http://localhost:9000')
+		print('Healthcheck ok')
+		break
+	except Exception as e:
+		print('Healthcheck failed')
+		print(e)
+		if retries > 20:
+			raise Exception('Healthcheck failed!')
+
+		time.sleep(2)
+		retries += 1
