fe15c2d1b151ae14d34d31a6927be7ef64b9296a
mikitane <miika.luiro@etasku.fi>
2020-11-20 17:08:52 +0200

    Fix APIGateway network alias

--- tests/test_apigateway.py
+++ tests/test_apigateway.py
@@ -4,7 +4,7 @@ import requests
 
 class APIGatewayTestCase(unittest.TestCase):
     def test_messages(self):
-        r = requests.get('http://apigateway:8081')
+        r = requests.get('http://apigateway_service:8081')
         content = r.content.decode('utf-8')
 
         expected = (
