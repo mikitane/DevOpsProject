7aacf0d5157ae042aa2a3a2f51e487df69533726
mikitane <miika.luiro@etasku.fi>
2020-11-20 13:50:28 +0200

    WIP

--- tests/test_httpserv.py
+++ tests/test_httpserv.py
@@ -6,7 +6,8 @@ class TestOutput(unittest.TestCase):
     def test_output(self):
         r = requests.get('http://httpserv_service:8082')
         content = r.content.decode('utf-8')
-
+        print("Test output")
+        print(content)
         expected = (
             r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z Topic my\.o: MSG_1\n'
             r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z Topic my\.i: Got MSG_1\n'
