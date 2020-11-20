eb067769450a614d38a7c54c880fadf30e2e9b94
mikitane <miika.luiro@etasku.fi>
2020-11-20 12:13:59 +0200

    WIP

--- tests/healthcheck.py
+++ tests/healthcheck.py
@@ -6,7 +6,7 @@ while True:
 	print("ASD")
 
 	try:
-		requests.get('http://httpserv_service_test:8082')
+		requests.get('http://httpserv_service:8082')
 		print('Healthcheck ok')
 		break
 	except Exception as e:
