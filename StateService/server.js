bbb585e558f60d966d4f87cd269ce15c600a8d7c
mikitane <miika.luiro@etasku.fi>
2020-11-21 15:46:53 +0200

    Implemented SHUTDOWN state

--- StateService/server.js
+++ StateService/server.js
@@ -1,96 +1,109 @@
 const http = require('http');
 const fs = require('fs');
+const { exec } = require('child_process');
 
 const STATE_FILE_PATH = '/state/state.txt';
 const STATE_LOG_FILE_PATH = '/state/state_log.txt';
 const MESSAGES_FILE_PATH = '/output/logs.txt';
 const PORT = 8083;
-const VALID_STATES = ['INIT', 'PAUSED', 'RUNNING', 'SHUTDOWN']
-
+const VALID_STATES = ['INIT', 'PAUSED', 'RUNNING', 'SHUTDOWN'];
 
 const getState = () => {
   return fs.readFileSync(STATE_FILE_PATH);
-}
+};
 
-const writeLog = newState => {
-  const timestamp = (new Date()).toISOString();
+const writeLog = (newState) => {
+  const timestamp = new Date().toISOString();
   fs.writeFileSync(STATE_LOG_FILE_PATH, `${timestamp}: ${newState}`);
-}
+};
 
-const initialize = () =>  {
+const initialize = () => {
   fs.writeFileSync(STATE_FILE_PATH, 'INIT');
   fs.writeFileSync(STATE_LOG_FILE_PATH, '');
   fs.writeFileSync(MESSAGES_FILE_PATH, '');
   writeLog('INIT');
+};
 
-  // if (!fs.existsSync(STATE_FILE_PATH)) {
-  // }
+const shutdown = () => {
+  fs.writeFileSync(STATE_FILE_PATH, 'SHUTDOWN');
+  writeLog('SHUTDOWN');
 
-  // if (!fs.existsSync(STATE_LOG_FILE_PATH)) {
+  const dockerComposeFile = process.env.DOCKER_COMPOSE_FILE;
 
-  // }
-}
+  exec(`docker-compose -f /devopsproject/${dockerComposeFile} stop`, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+    }
+    console.log(`stdout: ${stdout}`);
+});
+};
 
 const handleGetState = (req, res) => {
   const state = getState();
   res.statusCode = 200;
   return res.end(state);
-}
-
-const handleInitState = () => {
-
-}
+};
 
 const handlePutState = (req, res) => {
   const newState = req.data;
 
   let body = '';
-  req.on('data', chunk => {
-      body += chunk.toString();
+  req.on('data', (chunk) => {
+    body += chunk.toString();
   });
   req.on('end', () => {
-      const newState = body;
+    const newState = body;
 
-      if (!VALID_STATES.includes(newState)) {
-        res.statusCode = 400;
-        return res.end('Invalid state!')
-      }
+    if (!VALID_STATES.includes(newState)) {
+      res.statusCode = 400;
+      return res.end('Invalid state!');
+    }
 
-      const currentState = getState();
+    const currentState = getState();
 
-      // No effects if states are same
-      if (currentState === newState) {
-        res.statusCode = 200;
-        return res.end(currentState);
-      }
+    // No effects if states are same
+    if (currentState === newState) {
+      res.statusCode = 200;
+      return res.end(currentState);
+    }
 
-      if (newState === 'INIT') {
+    switch (newState) {
+      case 'INIT':
         initialize();
-      } else {
+        break;
+
+      case 'SHUTDOWN':
+        shutdown();
+        break;
+
+      default:
         fs.writeFileSync(STATE_FILE_PATH, newState);
         writeLog(newState);
-      }
+        break;
+    }
 
-      res.statusCode = 200;
-      return res.end(newState);
+    res.statusCode = 200;
+    return res.end(newState);
   });
-}
+};
 
 const server = http.createServer((req, res) => {
-  const { method, url, headers } = req
+  const { method, url, headers } = req;
 
-  if (method === "GET") {
+  if (method === 'GET') {
     return handleGetState(req, res);
   }
 
-  if (method === "PUT") {
+  if (method === 'PUT') {
     return handlePutState(req, res);
   }
 });
 
-
 initialize();
 
 server.listen(PORT, () => {
-  console.log(`HttpServ running at port ${PORT}`);
+  console.log(`StateService running at port ${PORT}`);
 });
