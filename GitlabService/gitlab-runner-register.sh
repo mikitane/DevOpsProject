c3c0d099df556feefebe92876bf4691bebc6558b
mikitane <miika.luiro@etasku.fi>
2020-11-25 09:25:54 +0200

    WIP

--- GitlabService/gitlab-runner-register.sh
+++ GitlabService/gitlab-runner-register.sh
@@ -14,4 +14,5 @@ docker exec -it gitlab-runner1 \
     --executor docker \
     --docker-image docker:stable \
     --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
+    --docker-volumes "./:/devopsproj" \
     --docker-network-mode gitlab-network
