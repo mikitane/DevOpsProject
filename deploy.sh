8cafcd8b1ecf072e99d7142fda6b2744fb2bfe46
mikitane <miika.luiro@etasku.fi>
2020-11-26 18:33:17 +0200

    Add deploy script for local and remote deployment

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+
+PRODUCTION_USER="devops_deploy"
+PRODUCTION_HOST="34.74.12.236"
+PRODUCTION_SSH_PRIVATE_KEY_FILE="deploy_key.txt"
+PRODCUTION_PROJECT_PATH="/home/devops_deploy/DevOpsProject"
+
+SSH_COMMAND="ssh -o \"StrictHostKeyChecking no\" -i $PRODUCTION_SSH_PRIVATE_KEY_FILE ${PRODUCTION_USER}@${PRODUCTION_HOST}"
+
+
+if [ "$DEPLOY_ENV" = "prod" ]
+then
+  echo "Production deployment"
+  eval $SSH_COMMAND "\"cd $PRODCUTION_PROJECT_PATH; docker-compose down -v\""
+  eval $SSH_COMMAND "\"cd $PRODCUTION_PROJECT_PATH; docker-compose up --build --force-recreate -d\""
+else
+  echo "Local deployment"
+  docker-compose down -v
+  docker-compose up --build --force-recreate -d
+fi
