8d8040a68abcdb98278a17aa158c4aa9d74250f5
mikitane <miika.luiro@etasku.fi>
2020-11-27 16:12:12 +0200

    Impl for /node-statistic GET

--- StateService/server.js
+++ StateService/server.js
@@ -5,7 +5,12 @@ const { exec } = require('child_process');
 const STATE_FILE_PATH = '/state/state.txt';
 const STATE_LOG_FILE_PATH = '/state/state_log.txt';
 const MESSAGES_FILE_PATH = '/output/logs.txt';
+
+const BROKER_SERVICE_HOSTNAME = 'broker_service';
+const BROKER_SERVICE_AUTH = 'guest:guest';
+const BROKER_SERVICE_PORT = 15672;
 const PORT = 8083;
+
 const VALID_STATES = ['INIT', 'PAUSED', 'RUNNING', 'SHUTDOWN'];
 
 const getState = () => {
@@ -57,8 +62,6 @@ const handleGetState = (req, res) => {
 };
 
 const handlePutState = (req, res) => {
-  const newState = req.data;
-
   let body = '';
   req.on('data', (chunk) => {
     body += chunk.toString();
@@ -103,7 +106,56 @@ const handleGetRunLog = (req, res) => {
   const logs = fs.readFileSync(STATE_LOG_FILE_PATH);
   res.statusCode = 200;
   return res.end(logs);
-}
+};
+
+const handleGetStatistics = (req, res) => {
+  const options = {
+    hostname: BROKER_SERVICE_HOSTNAME,
+    port: BROKER_SERVICE_PORT,
+    path: '/api/nodes',
+    method: 'GET',
+    auth: BROKER_SERVICE_AUTH,
+  };
+
+  const rabbitMqReq = http.request(options, (rabbitMqRes) => {
+    let rabbitMqData = '';
+
+    rabbitMqRes.on('data', (chunk) => {
+      rabbitMqData += chunk.toString();
+    });
+
+    rabbitMqRes.on('end', () => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+
+      try {
+        const rabbitMqJson = JSON.parse(rabbitMqData);
+
+        const keys = [
+          'fd_used',
+          'disk_free',
+          'mem_used',
+          'processors',
+          'io_read_avg_time',
+        ];
+
+        const responseData = Object.fromEntries(
+          keys.map((key) => [key, rabbitMqJson[0][key]])
+        );
+
+        res.end(JSON.stringify(responseData));
+      } catch (e) {
+        res.end('{}');
+      }
+    });
+  });
+
+  rabbitMqReq.on('error', err => {
+    res.end('{}');
+  });
+
+  rabbitMqReq.end();
+};
 
 const server = http.createServer((req, res) => {
   const { method, url, headers } = req;
@@ -118,6 +170,10 @@ const server = http.createServer((req, res) => {
 
   if (method === 'GET' && url === '/run-log') {
     return handleGetRunLog(req, res);
+  }
+
+  if (method === 'GET' && url === '/node-statistic') {
+    return handleGetStatistics(req, res);
   }
 });
 
