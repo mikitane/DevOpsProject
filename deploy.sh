50b4d6208f819678d92799b12d6e9a7ccca62de4
mikitane <miika.luiro@etasku.fi>
2020-12-03 19:18:51 +0200

    Change deploy env to local

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 
-PRODUCTION_USER="devops_deploy"
-PRODUCTION_HOST="34.74.12.236"
-PRODUCTION_SSH_PRIVATE_KEY_FILE="/etc/ssh/devops_deploy_key.txt"
-PRODCUTION_PROJECT_PATH="/home/devops_deploy/DevOpsProject"
+REMOTE_USER="devops_deploy"
+REMOTE_HOST="34.74.12.236"
+REMOTE_SSH_PRIVATE_KEY_FILE="/etc/ssh/devops_deploy_key.txt"
+REMOTE_PROJECT_PATH="/home/devops_deploy/DevOpsProject"
 
-SSH_COMMAND="ssh -o \"StrictHostKeyChecking no\" -i $PRODUCTION_SSH_PRIVATE_KEY_FILE ${PRODUCTION_USER}@${PRODUCTION_HOST}"
+SSH_COMMAND="ssh -o \"StrictHostKeyChecking no\" -i $REMOTE_SSH_PRIVATE_KEY_FILE ${REMOTE_USER}@${REMOTE_HOST}"
 
 
-if [ "$DEPLOY_ENV" = "prod" ]
+if [ "$DEPLOY_ENV" = "remote" ]
 then
   ls -l
   pwd
   echo "Production deployment"
-  chmod 600 $PRODUCTION_SSH_PRIVATE_KEY_FILE
-  eval $SSH_COMMAND "\"cd $PRODCUTION_PROJECT_PATH; docker-compose down -v\""
-  eval $SSH_COMMAND "\"cd $PRODCUTION_PROJECT_PATH; docker-compose up --build --force-recreate -d\""
+  chmod 600 $REMOTE_SSH_PRIVATE_KEY_FILE
+  eval $SSH_COMMAND "\"cd $REMOTE_PROJECT_PATH; docker-compose down -v\""
+  eval $SSH_COMMAND "\"cd $REMOTE_PROJECT_PATH; docker-compose up --build --force-recreate -d\""
 else
   echo "Local deployment"
   docker-compose down -v
